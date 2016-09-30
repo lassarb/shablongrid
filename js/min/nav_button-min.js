4a9103ae46c4f1613fe933547b5eb4c8e0ed8401
lassarb <lbalal@mail.ru>
2016-09-30 12:46:48 +0300

    hidden_button

---
+++ js/min/nav_button-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".button_hidden").click(function(){$(".navbar ul").slideToggle()})});
---
+++ js/nav_button.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".button_hidden").click(function() {
+        $(".navbar ul").slideToggle();
+    });
+});
